37e34e7f6b6b141c64cfbe6e8ba3aafa120cf908
Sri <110736434+Srikrishnadanda@users.noreply.github.com>
2026-07-14 08:33:39 +0530

    my secound commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("git is added");
+        System.out.println("git is added and made the change");
     }
 }
+clear
